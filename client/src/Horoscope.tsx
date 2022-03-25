c87400fc312d03cf7e5f8f7e6bc8eb31251ad8ef
Nicholas Bottone <nick@bottone.io>
2022-03-24 21:35:43 -0400

    Implement task 2

--- client/src/Horoscope.tsx
+++ client/src/Horoscope.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import TextBox from "./TextBox";
 
 export default function Horoscope() {
+  const [sun, setSun] = React.useState("");
+  const [moon, setMoon] = React.useState("");
+  const [rising, setRising] = React.useState("");
+
   return (
     <div>
       <h1>Horoscope React Lab!</h1>
-      <TextBox label="Sun" />
-      <TextBox label="Moon" />
-      <TextBox label="Rising" />
+      <TextBox label="Sun" value={sun} setValue={setSun} />
+      <TextBox label="Moon" value={moon} setValue={setMoon} />
+      <TextBox label="Rising" value={rising} setValue={setRising} />
     </div>
   );
 }
--- client/src/TextBox.tsx
+++ client/src/TextBox.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 
-export default function TextBox({ label }: { label: string }) {
-  return <input type="text" placeholder={label} />;
+export default function TextBox({
+  label,
+  value,
+  setValue,
+}: {
+  label: string;
+  value: string;
+  setValue: (value: string) => void;
+}) {
+  return (
+    <input
+      type="text"
+      placeholder={label}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
 }
